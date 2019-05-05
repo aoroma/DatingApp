16491efcc3f634f7a4340950b73b2323720a314d
aoroma <adolfo.o.romero.maya@gmail.com>
2019-05-05 10:22:57 -0500

    End of section 12

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -10,5 +11,29 @@ namespace DatingApp.API.Dtos
         [Required]
         [StringLength(10, MinimumLength = 8, ErrorMessage = "La contraseña debe de ser entre 8 y 10 caracteres")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOgBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
+
     }
 }
