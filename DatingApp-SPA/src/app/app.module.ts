a2c3b6e7b5ca8c1cf660515d3beaff92511ef001
aoroma <adolfo.o.romero.maya@gmail.com>
2019-05-10 10:13:03 -0500

    End of section 15

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListsResolver } from './_resolvers/list.resolver';
 
 // This Ok in Angular 7
 export function tokenGetter() {
@@ -81,7 +82,8 @@ export function tokenGetter() {
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
-      PreventUnsavedChanges
+      PreventUnsavedChanges,
+      ListsResolver
    ],
    bootstrap: [
       AppComponent
--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -17,5 +17,8 @@ namespace DatingApp.API.Helpers
         public int MaxAge { get; set; } = 99;
 
         public string OrderBy { get; set; }
+
+        public bool Likess { get; set; } = false;
+        public bool Likers { get; set; } = false;
     }
 }
